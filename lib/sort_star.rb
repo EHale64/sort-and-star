67814777011136067dfe8f1026319bf65f33ff23
Eric Hale <eric_hale@comcast.net>
2021-09-23 12:41:20 -0600

    second solution

--- lib/sort_star.rb
+++ lib/sort_star.rb
@@ -1,12 +1,13 @@
 class SortStar
     def two_sort(s)
-        sorted = s.sort
-        sorted.first.gsub(/./) do |letter|
-            if letter == sorted.first[-1]
-                letter
-            else
-                letter + '***'
-            end
-        end
+        s.sort.first.chars.join('***')
+        # sorted = s.sort
+        # sorted.first.gsub(/./) do |letter|
+        #     if letter == sorted.first[-1]
+        #         letter
+        #     else
+        #         letter + '***'
+        #     end
+        # end
     end
 end
